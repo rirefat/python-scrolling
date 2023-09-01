35c3515e3ab93de5475566a5668cd32ad247fa63
rirefat <rirefat.official@gmail.com>
2023-09-01 23:13:42 +0600

    able to open web cam

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("testing project")
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
